8d4e7f888a6c35bab1899f495824099f1b117354
Ratthapol Sakonchaiphraisan <ratthapol.sk@gmail.com>
2024-11-22 13:48:57 +0700

    fix: update next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'standalone', // Add this line to enable standalone build
+  reactStrictMode: true,
+  distDir: 'build',
   /* config options here */
 };
 
